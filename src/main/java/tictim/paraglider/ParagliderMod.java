27819cd8aec4dfa2e1f3f781e65c4600ba723d21
Tictim <ksw07149@gmail.com>
2022-11-15 15:06:25 +0900

    removing color from paragliders and deku leaves using cauldron
    (cherry picked from commit 088f391af16134465dd2fe806e97bc085444e66c)

--- src/main/java/tictim/paraglider/ParagliderMod.java
+++ src/main/java/tictim/paraglider/ParagliderMod.java
@@ -4,6 +4,7 @@ import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.MenuScreens.ScreenConstructor;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.client.renderer.item.ItemPropertyFunction;
+import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
@@ -52,7 +53,11 @@ public class ParagliderMod{
 
 	@SubscribeEvent
 	public static void setup(FMLCommonSetupEvent event){
-		event.enqueueWork(() -> ModVillageStructures.addVillageStructures());
+		event.enqueueWork(() -> {
+			ModVillageStructures.addVillageStructures();
+			CauldronInteraction.WATER.put(Contents.PARAGLIDER.get(), CauldronInteraction.DYED_ITEM);
+			CauldronInteraction.WATER.put(Contents.DEKU_LEAF.get(), CauldronInteraction.DYED_ITEM);
+		});
 	}
 
 	@SubscribeEvent
