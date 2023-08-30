3fa8135169f45d83b4c4dbae42bec62773cef25d
Tictim <ksw07149@gmail.com>
2023-08-31 00:29:12 +0900

    DisableStaminaRender docs

--- common/src/main/java/tictim/paraglider/client/screen/DisableStaminaRender.java
+++ common/src/main/java/tictim/paraglider/client/screen/DisableStaminaRender.java
@@ -1,3 +1,7 @@
 package tictim.paraglider.client.screen;
 
+/**
+ * Marker interface for {@link net.minecraft.client.gui.screens.Screen Screen}s that disables in-game stamina wheel
+ * rendering if a screen that implements this is open.
+ */
 public interface DisableStaminaRender{}
