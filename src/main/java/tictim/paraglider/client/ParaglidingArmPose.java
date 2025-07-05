3abdd3cb11b31901399cc0e18378597cb490bc22
Tictim <ksw07149@gmail.com>
2025-07-05 12:11:15 +0900

    This commit does not exist

--- src/main/java/tictim/paraglider/client/ParaglidingArmPose.java
+++ src/main/java/tictim/paraglider/client/ParaglidingArmPose.java
@@ -8,7 +8,10 @@ import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.fml.common.asm.enumextension.EnumProxy;
 import net.neoforged.neoforge.client.IArmPoseTransformer;
+import org.jetbrains.annotations.Nullable;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
@@ -22,13 +25,45 @@ public final class ParaglidingArmPose {
 
 	private static final float ARM_ROTATION = (float)(Math.PI * 2 - 2.9);
 
+	private static @Nullable Boolean IS_CHRISTMAS;
+
 	private static void applyTransform(HumanoidModel<?> model, LivingEntity entity, HumanoidArm arm) {
-		model.leftArm.xRot = ARM_ROTATION;
-		model.leftArm.zRot = 0;
-		model.rightArm.xRot = ARM_ROTATION;
-		model.rightArm.zRot = 0;
-		model.leftLeg.xRot = 0f;
-		model.rightLeg.xRot = 0f;
+		Boolean theDay = IS_CHRISTMAS;
+		if (theDay == null) {
+			LocalDate now = LocalDate.now();
+			IS_CHRISTMAS = theDay = now.getMonth() == Month.APRIL && now.getDayOfMonth() == 1;
+		}
+
+		if (theDay) {
+			model.head.xRot = 0;
+			model.head.yRot = 0;
+			model.head.zRot = 0;
+			model.leftArm.xRot = 0;
+			model.leftArm.yRot = 0;
+			model.leftArm.zRot = (float)(-Math.PI / 2);
+			model.rightArm.xRot = 0;
+			model.rightArm.yRot = 0;
+			model.rightArm.zRot = (float)(Math.PI / 2);
+			model.leftLeg.xRot = 0;
+			model.leftLeg.yRot = 0;
+			model.leftLeg.zRot = 0;
+			model.rightLeg.xRot = 0;
+			model.rightLeg.yRot = 0;
+			model.rightLeg.zRot = 0;
+		} else {
+			model.leftArm.xRot = ARM_ROTATION;
+			model.leftArm.yRot = 0;
+			model.leftArm.zRot = 0;
+			model.rightArm.xRot = ARM_ROTATION;
+			model.rightArm.yRot = 0;
+			model.rightArm.zRot = 0;
+			model.leftLeg.xRot = 0;
+			model.leftLeg.yRot = 0;
+			model.leftLeg.zRot = 0;
+			model.rightLeg.xRot = 0;
+			model.rightLeg.yRot = 0;
+			model.rightLeg.zRot = 0;
+		}
 
 		Minecraft mc = Minecraft.getInstance();
 		ClientLevel level = Objects.requireNonNull(mc.level);
